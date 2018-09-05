1aafeab965670da6591908f202c1eb738c914181
eddy <edward@edwardvo.ca>
2018-09-05 09:51:24 -0700

    TODO: Update IcyWind.Chat and fix logins

---
+++ IcyWind.Chat/TcpStringSocket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcyWind.Chat
+{
+    /// <summary>
+    /// This class sets up a socket
+    /// This socket connects to a given xmpp server and invokes certain events
+    /// This event should return a bool, being true if the message is a full xmpp message
+    /// </summary>
+    public class TcpStringSocket
+    {
+        
+        private TcpStringSocket(IPEndPoint hostIp)
+        {
+            
+        }
+    }
+}
