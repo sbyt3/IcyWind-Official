4b362629103bc565de7d8e4de9c50c85cabfd0e9
eddy5641 <edward@edwardvo.ca>
2018-10-10 16:09:47 -0700

    Minor changes

---
+++ IcyWind.Injector/InjectSocket/NonLoopbackIPAddressException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcyWind.Injector.InjectSocket
+{
+    public class NonLoopbackIPAddressException : Exception
+    {
+        public NonLoopbackIPAddressException() { }
+
+        public NonLoopbackIPAddressException(string msg) : base(msg) { }
+
+        public NonLoopbackIPAddressException(string msg, Exception ex) : base(msg, ex) { }
+    }
+}
