f959dbca20864ee2d216de3c12a497e5ba15e8cd
eddy <edward@edwardvo.ca>
2018-10-10 15:52:04 -0700

    Updates

--- IcyWind.Injector/Injector/InjectDX9.cs
+++ IcyWind.Injector/Injector/InjectDX9.cs
@@ -5,49 +5,54 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using IcyWind.Injector.Injector;
 
 namespace IcyWind.ExtScript.Injector
 {
     // ReSharper disable once InconsistentNaming
     public class InjectDX9
     {
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
-        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,
-            uint dwSize, uint flAllocationType, uint flProtect);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
-
-        [DllImport("kernel32.dll")]
-        static extern IntPtr CreateRemoteThread(IntPtr hProcess,
-            IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
-
-
-        const int PROCESS_CREATE_THREAD = 0x0002;
-        const int PROCESS_QUERY_INFORMATION = 0x0400;
-        const int PROCESS_VM_OPERATION = 0x0008;
-        const int PROCESS_VM_WRITE = 0x0020;
-        const int PROCESS_VM_READ = 0x0010;
-
-
-        private Process _targetProcess { get; }
+        public Process TargetProcess { get; }
+        public IntPtr ProcHandle { get; private set; }
 
         public InjectDX9(Process targetProcess)
         {
-            _targetProcess = targetProcess;
+            TargetProcess = targetProcess;
         }
 
-        public bool InjectDll(string dllLocation)
+        public bool InjectDll(string dllName)
         {
+            ProcHandle = Kernal32Native.OpenProcess(Kernal32Native.PROCESS_CREATE_THREAD |
+                                                    Kernal32Native.PROCESS_QUERY_INFORMATION |
+                                                    Kernal32Native.PROCESS_VM_OPERATION |
+                                                    Kernal32Native.PROCESS_VM_READ |
+                                                    Kernal32Native.PROCESS_VM_WRITE,
+                false,
+                TargetProcess.Id);
+
+
+            var loadLibraryAddress = Kernal32Native.GetProcAddress(Kernal32Native.GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+            var allocMemAddress = Kernal32Native.VirtualAllocEx(ProcHandle, 
+                IntPtr.Zero, 
+                (uint)((dllName.Length + 1) * Marshal.SizeOf(typeof(char))),
+                Kernal32Native.MEM_COMMIT |
+                Kernal32Native.MEM_RESERVE,
+                Kernal32Native.PAGE_READWRITE);
+
+
+            Kernal32Native.WriteProcessMemory(ProcHandle, 
+                allocMemAddress, 
+                Encoding.Default.GetBytes(dllName), 
+                (uint)((dllName.Length + 1) * Marshal.SizeOf(typeof(char))), 
+                out var bytesWritten);
+
+            Kernal32Native.CreateRemoteThread(ProcHandle, 
+                IntPtr.Zero, 
+                0, 
+                loadLibraryAddress, 
+                allocMemAddress, 
+                0, 
+                IntPtr.Zero);
 
             return true;
         }
